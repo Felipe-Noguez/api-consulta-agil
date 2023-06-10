01039e06ab351cd83a17fdedf769361022baf096
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-06-10 07:49:19 -0300

    Estrutura do projeto

---
+++ src/main/java/com/agil/consultas/apiconsultaagil/ApiConsultaAgilApplication.java
@@ -0,0 +1,13 @@
+package com.agil.consultas.apiconsultaagil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaAgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaAgilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agil/consultas/apiconsultaagil/ApiConsultaAgilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agil.consultas.apiconsultaagil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaAgilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
