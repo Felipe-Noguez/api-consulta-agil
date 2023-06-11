09c892f6332e17bb494275a79cc2a2252c115907
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-06-11 10:42:21 -0300

    Fix: Correção no GenerationType de Paciente

--- src/main/java/com/agil/consultas/apiconsultaagil/entities/PacienteEntity.java
+++ src/main/java/com/agil/consultas/apiconsultaagil/entities/PacienteEntity.java
@@ -17,6 +17,7 @@ import java.util.Set;
 public class PacienteEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_paciente", nullable = false)
     private Long idPaciente;
 
