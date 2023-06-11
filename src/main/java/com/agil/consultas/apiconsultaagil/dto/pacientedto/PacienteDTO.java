ac244873d2ed6e11304f05b372edb0eb2389d87f
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-06-11 10:14:31 -0300

    Feat: Adicionado DTOs para paciente

---
+++ src/main/java/com/agil/consultas/apiconsultaagil/dto/pacientedto/PacienteDTO.java
@@ -0,0 +1,24 @@
+package com.agil.consultas.apiconsultaagil.dto.pacientedto;
+
+import com.agil.consultas.apiconsultaagil.entities.PacienteEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PacienteDTO {
+
+    private Long idCliente;
+
+    private String nome;
+
+    private String telefone;
+
+
+    public PacienteDTO(PacienteEntity pacienteEntity) {
+        BeanUtils.copyProperties(pacienteEntity, this);
+    }
+}
