6c61d4a331aaf4391c46381ae349549ecfec4182
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-06-11 18:28:53 -0300

    Fix: Correção em data e no GenerationType de Consulta

--- src/main/java/com/agil/consultas/apiconsultaagil/entities/ConsultaEntity.java
+++ src/main/java/com/agil/consultas/apiconsultaagil/entities/ConsultaEntity.java
@@ -1,12 +1,13 @@
 package com.agil.consultas.apiconsultaagil.entities;
 
 import com.agil.consultas.apiconsultaagil.entities.pk.PacienteConsultaPK;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Set;
 
@@ -17,14 +18,18 @@ import java.util.Set;
 public class ConsultaEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_consulta")
     private Long idConsulta;
 
     @Column(name = "especialidade")
     private String especialidade;
 
+    @JsonFormat(pattern="dd-MM-yyyy HH:mm:ss")
+    @DateTimeFormat(pattern="dd-MM-yyyy HH:mm:ss")
+    @Column(name = "data_hora_consulta")
     private LocalDateTime dataHoraConsulta;
 
-    @OneToMany(mappedBy = "consultaEntity", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "consultaEntity", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<PacienteConsultaPK> pacienteConsultaPK;
 }
