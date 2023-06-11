3583035a3ded44ed08d4330373df0126e51f64da
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-06-11 18:31:19 -0300

    Fix: Adicionado validação no PacienteCreateDTO e documentação do DTO

--- src/main/java/com/agil/consultas/apiconsultaagil/dto/pacientedto/PacienteCreateDTO.java
+++ src/main/java/com/agil/consultas/apiconsultaagil/dto/pacientedto/PacienteCreateDTO.java
@@ -1,15 +1,23 @@
 package com.agil.consultas.apiconsultaagil.dto.pacientedto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class PacienteCreateDTO {
 
+    @NotBlank(message = "campo obrigatório, preencha e tente novamente")
+    @NotNull(message = "campo obrigatório, preencha e tente novamente")
+    @Schema(description = "Nome do paciente para o cadastro", example = "Mike")
     private String nome;
 
+    @Schema(description = "Telefone do paciente para o cadastro", example = "(51) 99999-1234")
     private String telefone;
 }
